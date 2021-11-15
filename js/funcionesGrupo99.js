7063ada5ac819aaa256d508d8b7f1b409759d575
= <=>
2021-11-15 17:53:14 -0300

    cambios4

---
+++ js/funcionesGrupo99.js
@@ -0,0 +1,16 @@
+
+   
+        function bigImg(x) {
+          x.style.height = "94px";
+          x.style.width = "94px";
+        }
+        
+        function normalImg(x) {
+          x.style.height = "62px";
+          x.style.width = "62px";
+        }
+        
+    
+    function alerta (){
+        alert ("Operacion exitosa!!");
+        }  
