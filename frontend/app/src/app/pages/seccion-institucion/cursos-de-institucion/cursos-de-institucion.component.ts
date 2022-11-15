132d8d5bea7073663061f570f914196342749cbf
Juan López <juanem@hotmail.com>
2022-11-14 23:57:45 -0300

    delete and put course information

--- frontend/app/src/app/pages/seccion-institucion/cursos-de-institucion/cursos-de-institucion.component.ts
+++ frontend/app/src/app/pages/seccion-institucion/cursos-de-institucion/cursos-de-institucion.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit,  } from '@angular/core';
 import { Course } from '../../../models/Course';
 import { InstitutionService } from 'src/app/services/institution.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { CommerceHeaderComponent } from 'src/app/layout/commerce-header/commerce-header.component';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-cursos-de-institucion',
@@ -11,11 +14,21 @@ export class CursosDeInstitucionComponent implements OnInit {
 
   listCourse: Course[] = [];
   cInscriptos = 0;
-  constructor(private institutionService:InstitutionService) { }
+  idCourse:number;
+
+  constructor(private institutionService:InstitutionService,
+    private auth: AuthService){}
 
   ngOnInit(): void {
     this.loadCourses();
   }
+
+  curso = new FormGroup({
+    name: new FormControl('CURSO NUEVO', Validators.required),
+    description: new FormControl('DETALLE', Validators.required),
+    beginningDay: new FormControl('12-12-2020', Validators.required),
+    endingDay: new FormControl('12/12/2022', Validators.required)
+  });
 
   loadCourses(){
     return this.institutionService.getAllCoursesById().subscribe(data=>{
@@ -31,6 +44,48 @@ export class CursosDeInstitucionComponent implements OnInit {
     alert("Curso eliminado correctamente!");
   }
   editar(id:number){
-    alert("curso creado con éxito");
+    this.idCourse = id;
+    this.institutionService.getCourses(id).subscribe((data)=>{
+      this.curso.get('name').setValue(data.name);
+      this.curso.get('description').setValue(data.description);
+      this.curso.get('beginningDay').setValue(data.beginningDay);
+      this.curso.get('endingDay').setValue(data.endingDay);
+      })
+  }
+  editarCurso(){
+    let name = this.curso.value.name;
+    let description = this.curso.value.description;
+    let beginningDay = this.curso.value.beginningDay;
+    let endingDay = this.curso.value.endingDay;
+
+    if (this.curso.invalid)
+      return alert("faltan datos");
+
+   const datos = new Course(name,description,beginningDay,endingDay);
+   this.institutionService.editCourse(this.idCourse, datos)
+   .subscribe((data)=>{
+    console.log(data.id)
+  });
+   return alert("CURSO MODIFICADO");
+  }
+
+
+  crearCurso(){
+    let name = this.curso.value.name;
+    let description = this.curso.value.description;
+    let beginningDay = this.curso.value.beginningDay;
+    let endingDay = this.curso.value.endingDay;
+
+    if (this.curso.invalid)
+      return alert("faltan datos");
+
+   const datos = new Course(name,description,beginningDay,endingDay);
+
+  this.institutionService
+  .createCourse(datos)
+  .subscribe((data)=>{
+    console.log(data.id)
+  });
+   return alert("CURSO CREADO");
   }
 }
